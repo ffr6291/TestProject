b91ba24a7c6a757c866424ab8beb02ac333fa9dd
franz <franz@MSI.home>
2019-04-01 18:34:47 +0200

    test

--- Meldeamt/src/at/fhv/oo/Marriage.java
+++ Meldeamt/src/at/fhv/oo/Marriage.java
@@ -2,17 +2,14 @@ package at.fhv.oo;
 
 public class Marriage {
 
-	private boolean marrypossible = true;
-
-	public boolean Marriage(Person p1, Person p2) {
+	public Marriage(Person p1, Person p2) {
 
 		if (p1.getGender() != p2.getGender()) {
 			System.out.println("Heirat möglich");
 
-			return marrypossible;
+			
 		} else {
-			marrypossible = false;
-			return marrypossible;
+			System.out.println("Homoehe nicht möglich!!");
 		}
 	}
 }
